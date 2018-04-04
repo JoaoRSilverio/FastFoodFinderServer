37f4cab750ff8e366743521bd1abb6c3c384e2bc
Joao Silverio <joao.r.silverio@gmail.com>
2018-04-05 00:05:10 +0200

    removed

--- thefile.js
+++
@@ -1,6 +0,0 @@
-const MYKEYS = {
-  
-    DBPATH_LIVE : 'mongodb://fastadmin:*5opraFASTFOODFINDER*@ds012178.mlab.com:12178/fffdevelopment',
-    DBPATH_LOCAL : 'mongodb://localhost:27017/foodexplorerbeta'
-};
- module.exports = MYKEYS;
